59ae4f2042d35ee3ae115fd0cedc2453d5b68ec5
Tanjidul Hasan <tanjidulhasan1@gmail.com>
2025-06-12 06:18:38 +0600

    added 2 files

---
+++ day-1.sql
@@ -0,0 +1,72 @@
+
+
+insert into AFA.Player(JersyNo,FirstName,ClubName) values(10,'Messy','Inter Miami');
+insert into AFA.Player(JersyNo,FirstName,ClubName) values(13,'Romero','Tottenham');
+SELECT 
+    *
+FROM
+    AFA.Player;
+SELECT JersyNo,FirstName FROM AFA.Player;
+SELECT FirstName, ClubName 
+FROM AFA.Player
+WHERE ClubName='Inter Miami';
+ALTER TABLE AFA.Player
+ADD Age int;
+select *from AFA.PLayer;
+insert into AFA.Player(Age) values(37);
+insert into AFA.Player(Age) values(24);
+select *from AFA.Player;
+SET SQL_SAFE_UPDATES = 0;
+Alter table AFA.Player
+Add Position varchar(100);
+select *from AFA.Player
+DELETE FROM AFA.Player
+WHERE  JersyNo IS null; 
+select *from AFA.Player;
+set sql_safe_updates=0;
+update AFA.Player
+set Position="Forwards"
+where JersyNo=10;
+update AFA.Player
+set Position= "Defenders"
+where JersyNo=13;
+select *from AFA.Player;
+set sql_safe_updates=1;
+set sql_safe_updates=1;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE AFA.Player
+SET Age = 37
+WHERE JersyNo = 10;
+
+UPDATE AFA.Player
+SET Age = 24
+WHERE JersyNo = 13;
+select *from AFA.Player;
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(1,'Armani','River Plate',38,"Goalkeepers");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(12,'Rulli','Ajax',32,"Goalkeepers");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(23,'Emiliano Martinez','Aston Vila',32,"Goalkeepers");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(2,'Lucas Martinez','Fiorentina',29,"Defenders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(3,'Nicolas Tagliafico','Lyon',32,"Defenders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(4,'Gonazalo Montiel','Nottingham',28,"Defenders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(6,'Pezzella','Real Betis',33,"Defenders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(8,'Marcos Acuna','Sevilla',33,"Defenders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(19,'Nicolas Otamendi','Benfica',37,"Defenders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(25,'Lisandro Martinez','Manchester United',27,"Defenders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(26,'Nahuel Molina','Atletico Madrid',27,"Defenders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(5,'Leandro Paredes','Roma',30,"Midfielders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(7,'Rodrigo De Paul','Atletico Madrid',30,"Midfielders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(14,'Palacios','Bayer leverkusen',26,"Midfielders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(18,'Rodriguez','Real Betis',31,"Midfielders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(16,' Giovani Lo Celso','Tottenham',29,"Midfielders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(20,'Mac Allister ','Liverpool',26,"Midfielders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(24,' Enzo Fernandez','Chelsea',24,"Midfielders");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(9,'Julian Alvarez','Atletico Madrid',25,"Forwards");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(11,'Angel Di Maria','Benfica',37,"Forwards");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(15,'Nicolas Gonzalez','Fiorentina',27,"Forwards");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(17,'Alejandro Garnacho','Manchester United',20,"Forwards");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(21,'Carboni','Monza',20,"Forwards");
+insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(22,'Lautaro Martinez','Inter Milan',27,"Forwards");
+
+select *from AFA.Player
+select FirstName,Age from AFA.Player 
+where ClubName='Atletico Madrid';
