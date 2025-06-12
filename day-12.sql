123823d8329b3effbec71924c9620ec907e32004
Tanjidul Hasan <tanjidulhasan1@gmail.com>
2025-06-13 01:10:07 +0600

    updated day-12

--- day-12.sql
+++ day-12.sql
@@ -18,4 +18,12 @@ concat(first_name, " " ,last_name) Full_Name,
 gender
 from
 t_employees
-where gender="M"
+where gender="M";
+
+select 
+count(hire_year)
+from
+(select 
+year(hire_date) hire_year
+from t_employees)a
+where hire_year =2000
