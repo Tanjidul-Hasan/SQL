53f8dc33801fff260c90df101d12383a2c17894d
Tanjidul Hasan <tanjidulhasan1@gmail.com>
2025-06-13 22:51:20 +0600

    added some code on day-1

--- day-1.sql
+++ day-1.sql
@@ -1,5 +1,5 @@
 
-
+use afa
 insert into AFA.Player(JersyNo,FirstName,ClubName) values(10,'Messy','Inter Miami');
 insert into AFA.Player(JersyNo,FirstName,ClubName) values(13,'Romero','Tottenham');
 SELECT 
@@ -10,18 +10,26 @@ SELECT JersyNo,FirstName FROM AFA.Player;
 SELECT FirstName, ClubName 
 FROM AFA.Player
 WHERE ClubName='Inter Miami';
+
+-- add a new column
 ALTER TABLE AFA.Player
 ADD Age int;
+
 select *from AFA.PLayer;
 insert into AFA.Player(Age) values(37);
 insert into AFA.Player(Age) values(24);
 select *from AFA.Player;
 SET SQL_SAFE_UPDATES = 0;
+
+-- add a new column
 Alter table AFA.Player
 Add Position varchar(100);
+
 select *from AFA.Player
+
 DELETE FROM AFA.Player
 WHERE  JersyNo IS null; 
+
 select *from AFA.Player;
 set sql_safe_updates=0;
 update AFA.Player
@@ -31,17 +39,24 @@ update AFA.Player
 set Position= "Defenders"
 where JersyNo=13;
 select *from AFA.Player;
+
+-- on safe update mode
 set sql_safe_updates=1;
-set sql_safe_updates=1;
+
+-- stop safe update mode
 SET SQL_SAFE_UPDATES = 0;
+
+--update a row
 UPDATE AFA.Player
 SET Age = 37
 WHERE JersyNo = 10;
 
+-- update a row
 UPDATE AFA.Player
 SET Age = 24
 WHERE JersyNo = 13;
 select *from AFA.Player;
+
 insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(1,'Armani','River Plate',38,"Goalkeepers");
 insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(12,'Rulli','Ajax',32,"Goalkeepers");
 insert into AFA.Player(JersyNo,FirstName,ClubName,Age,Position)values(23,'Emiliano Martinez','Aston Vila',32,"Goalkeepers");
@@ -131,3 +146,93 @@ FROM
     player
 WHERE
     position in("midfielders")
+
+select count(jersyno)
+from player
+select firstname from player
+
+-- add a new column of world_cup_titels
+ALTER TABLE player ADD COLUMN world_cup_titles INT;
+select *from player
+
+-- fill/update this world_cup_titels column with data/update a whole column
+-- as we are using safe update mood so we need to stop it 
+set sql_safe_updates=0;
+UPDATE player
+SET world_cup_titles = CASE firstname
+    WHEN 'Messy' THEN 1
+    WHEN 'Romero' THEN 1
+    WHEN 'Armani' THEN 1
+    WHEN 'Lucas Martínez' THEN 0
+	WHEN 'Rulli'THEN 1
+    WHEN 'Emiliano Martinez'THEN 1
+    WHEN 'Nicolas Tagliafico'THEN 1
+    WHEN 'Gonazalo Montiel'THEN 1
+    WHEN 'Pezzella'THEN 1
+    WHEN 'Marcos Acuna' THEN 1
+    WHEN 'Nicolas Otamendi' THEN 1
+    WHEN 'Lisandro Martinez' THEN 1
+    WHEN 'Nahuel Molina' THEN 1
+    WHEN 'Leandro Paredes' THEN 1
+    WHEN 'Rodrigo De Paul' THEN 1
+    WHEN 'Palacios' THEN 1
+    WHEN 'Rodriguez' THEN 1
+    WHEN ' Giovani Lo Celso' THEN 1
+    WHEN 'Mac Allister' THEN 1
+    WHEN ' Enzo Fernandez' THEN 1
+    WHEN 'Julian Alvarez' THEN 1
+    WHEN 'Angel Di Maria' THEN 1
+    WHEN 'Nicolas Gonzalez' THEN 1
+	WHEN 'Alejandro Garnacho' THEN 0
+	WHEN 'Carboni' THEN 0
+    WHEN 'Lautaro Martinez' THEN 1
+    ELSE 0
+END;
+
+-- add a new column of ucl_titels
+ALTER TABLE player ADD COLUMN ucl_titles INT;
+select *from player
+
+-- fill/update this ucl_titels column with data
+-- as we are using safe update mood so we need to stop it 
+set sql_safe_updates=0;
+UPDATE player
+SET ucl_titles = CASE firstname
+    WHEN 'Messy' THEN 4
+    WHEN 'Romero' THEN 0
+    WHEN 'Armani' THEN 0
+    WHEN 'Lucas Martínez' THEN 0
+	WHEN 'Rulli'THEN 1
+    WHEN 'Emiliano Martinez'THEN 0
+    WHEN 'Nicolas Tagliafico'THEN 0
+    WHEN 'Gonazalo Montiel'THEN 1
+    WHEN 'Pezzella'THEN 0
+    WHEN 'Marcos Acuna' THEN 1
+    WHEN 'Nicolas Otamendi' THEN 0
+    WHEN 'Lisandro Martinez' THEN 0
+    WHEN 'Nahuel Molina' THEN 0
+    WHEN 'Leandro Paredes' THEN 0
+    WHEN 'Rodrigo De Paul' THEN 0
+    WHEN 'Palacios' THEN 0
+    WHEN 'Rodriguez' THEN 0
+    WHEN ' Giovani Lo Celso' THEN 0
+    WHEN 'Mac Allister' THEN 0
+    WHEN ' Enzo Fernandez' THEN 0
+    WHEN 'Julian Alvarez' THEN 1
+    WHEN 'Angel Di Maria' THEN 1
+    WHEN 'Nicolas Gonzalez' THEN 0
+	WHEN 'Alejandro Garnacho' THEN 0
+	WHEN 'Carboni' THEN 0
+    WHEN 'Lautaro Martinez' THEN 0
+    ELSE 0
+END;
+select *from player
+
+-- update a row info
+update Player
+set firstname="Lionel Messy"
+where JersyNo=10;
+
+-- change column name
+ALTER TABLE player CHANGE FirstName Full_Name VARCHAR(100);
+ALTER TABLE player CHANGE ClubName Club_Name VARCHAR(100);
