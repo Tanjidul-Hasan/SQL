7d6908f9f4652866ff78ada75b123c6ab69570cb
Tanjidul Hasan <tanjidulhasan1@gmail.com>
2025-06-17 11:17:09 +0600

    edited new file day-14 project filey

--- Day-14 (IMS PROJECT)/Day-14 project query part.sql
+++ Day-14 (IMS PROJECT)/Day-14 project query part.sql
@@ -1,11 +1,20 @@
 use ims
 
 -- Query-1: Check product stock levels.
+/*Description:
+Retrieves the current stock quantity for each product by joining the Inventory and Products tables on product_id.
+select p.product_name,i.quantity*/
 
-select p.product_name,i.quantity
 from
 (select product_id,Product_name from product)p
 join -- here 'join' act as a inner join
 (select product_id,quantity from inventory)i
 on p.product_id=i.product_id;
 
+/* Query 2: Update Stock Levels After Sale or Purchase
+Description:
+ Updates stock levels after a sale or a purchase.
+Sale: Decrease quantity by 3 for product_id = 2 (Desk).
+Purchase: Increase quantity by 10 for product_id = 1 (Laptop).
+ */
+ 
