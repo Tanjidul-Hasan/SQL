2d50cb810fa137f560541858fcb0d785c1cf30f5
Tanjidul Hasan <tanjidulhasan1@gmail.com>
2025-06-18 21:03:47 +0600

    upadated some code on IMS Project

--- Day-14 (IMS PROJECT)/Day-14 project query part.sql
+++ Day-14 (IMS PROJECT)/Day-14 project query part.sql
@@ -24,7 +24,25 @@ set sql_safe_updates=0
 update inventory
 set quantity = quantity-6
 where product_id =50295
+
 -- after purchage
 update inventory
 set quantity=quantity+6
 where product_id=50295
+
+/*Query 3: View Transaction History for a Product
+Description:
+Displays transaction type, quantity, and date for product_id = 1 (Laptop), showing only relevant
+transactions.*/
+select product_id,transaction_type,transaction_date,quantity
+from transaction
+where product_id=50295 -- and transaction_type='purchase'/'sale'
+
+-- if we want to know by product name(partial) and we don't know product_id and even full product name also in that case:
+select p.product_name,t.product_id,t.transaction_type,t.transaction_date,t.quantity
+from
+(select product_id,transaction_type,transaction_date,quantity from transaction)t
+join
+(select product_id, product_name from product)p
+on t.product_id=p.product_id
+where p.product_name like '%laptop%'
