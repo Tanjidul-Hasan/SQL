b2234b5bab74b03e60cebc8a3f42e29c6b1f46e0
Tanjidul Hasan <tanjidulhasan1@gmail.com>
2025-06-18 20:39:19 +0600

    upadated some code on IMS Project

--- Day-14 (IMS PROJECT)/Day-14 project query part.sql
+++ Day-14 (IMS PROJECT)/Day-14 project query part.sql
@@ -10,7 +10,7 @@ from
 join -- here 'join' act as a inner join
 (select product_id,quantity from inventory)i
 on p.product_id=i.product_id
--- where p.product_name like '%laptop%'
+where p.product_name like '%laptop%'
 
 
 /* Query 2: Update Stock Levels After Sale or Purchase
@@ -19,7 +19,12 @@ Description:
 Sale: Decrease quantity by 3 for product_id = 2 (Desk).
 Purchase: Increase quantity by 10 for product_id = 1 (Laptop).
  */
+ -- after sale
 set sql_safe_updates=0
 update inventory
-set quantity = quantity-3
-where product_id =2
+set quantity = quantity-6
+where product_id =50295
+-- after purchage
+update inventory
+set quantity=quantity+6
+where product_id=50295
